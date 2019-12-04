23686754407edc01353314acbdc92914201319e8
kpryzk <kepryzk@gmail.com>
2019-12-04 12:07:27 -0500

    do not merge

--- components/screens/singleQuiz.js
+++ components/screens/singleQuiz.js
@@ -11,6 +11,7 @@ import styles from '../../assets/styles/singleQuiz'
 import { gotOneQuiz } from '../../redux/actions/quizActions'
 import { connect } from 'react-redux'
 import { loadUser } from '../storage/userStorage'
+import { throws } from 'assert'
 
 class Quiz extends Component {
   static navigationOptions = {
@@ -27,17 +28,20 @@ class Quiz extends Component {
     super()
     this.state = {
       user: {},
-      quiz: {}, 
+      quiz: {},
       questions: [],
       totalQuestionCount: 0,
       correctCount: 0,
+      currentQuestion: {},
       activeQuestionIndex: 0,
       answered: false,
       answerCorrect: false,
     }
     this.answer = this.answer.bind(this)
     this.nextQuestion = this.nextQuestion.bind(this)
+    this.initialState = this.initialState.bind(this)
   }
+
   answer(userAnswer, testAnswer) {
     this.setState(
       state => {
@@ -56,21 +60,27 @@ class Quiz extends Component {
   nextQuestion(){
     this.setState(state => {
       const nextIndex = state.activeQuestionIndex + 1;
-
+       const current = state.questions[nextIndex]
       if (nextIndex >= state.totalQuestionCount) {
         this.props.navigation.popToTop();
       }
       return {
+        currentQuestion: current,
         activeQuestionIndex: nextIndex,
         answered: false
       };
     });
-  };
+  }
 
   async componentDidMount() {
+    await this.initialState()
+  }
+
+  async initialState() {
     this.user = await loadUser()
     const userId = this.user.id
     await this.props.getOneQuiz(userId)
+    this.setState({currentQuestion: this.props.quizInfo.quiz['test-questions'][0]})
     this.setState({questions: this.props.quizInfo.quiz['test-questions']} )
     const questionsLength = this.state.questions.length
     this.setState({totalQuestionCount: questionsLength})
@@ -78,10 +88,12 @@ class Quiz extends Component {
 
   render() {
     const currentQuiz = this.props.quizInfo.quiz
-    const currentIndex = this.state.activeQuestionIndex
-    const question = this.state.questions[currentIndex]
-    console.log('question', question)
-    console.log('currentIdx', currentIndex)
+    // const currentIndex = this.state.activeQuestionIndex
+    // console.log('QUESTIONS', this.SOMETHING)
+    // console.log('currentIdx', currentIndex)
+    // const questions = this.props.quizInfo.quiz['test-questions'];
+    // const question = questions[this.state.activeQuestionIndex];
+    console.log("QUESTIONS", this.state.currentQuestion)
 
     return (
       <ImageBackground
@@ -90,14 +102,15 @@ class Quiz extends Component {
         <ScrollView style={styles.container}>
         <View style={styles.container}>
             <Text style={styles.text}> {currentQuiz.description}</Text>
-                  <Text style={styles.text}>
+                  {/* <Text style={styles.text}>
+                    {}
                     </Text>
                   <TouchableOpacity onPress={()=>this.answer('Yes', question.answer)}>
                     <Text style={styles.text}>Yes</Text>
                   </TouchableOpacity>
                   <TouchableOpacity onPress={()=>this.answer('No', question.answer)}>
                   <Text style={styles.text}> No</Text>
-                  </TouchableOpacity>
+                  </TouchableOpacity> */}
             </View>
         </ScrollView>
       </ImageBackground>
